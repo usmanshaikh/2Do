fc514f7e4b76477af06fec281c6cb4bc666ebe61
Usman Shaikh <shaikhusman57@gmail.com>
2022-06-04 14:54:09 +0530

    components and pages created for Auth, Walkthrough

---
+++ client/2do/src/pages/Auth/ResetPassword/ResetPasswordSuccess.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ResetPasswordSuccess = () => {
+  return (
+    <>
+      <div>ResetPasswordSuccess</div>
+    </>
+  );
+};
+
+export default ResetPasswordSuccess;
---
+++ client/2do/src/pages/Walkthrough/Walkthrough.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Walkthrough = () => {
+  return (
+    <>
+      <div>Walkthrough</div>
+    </>
+  );
+};
+
+export default Walkthrough;
