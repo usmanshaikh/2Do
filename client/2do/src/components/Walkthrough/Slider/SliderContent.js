8630ab41faa1366152848f25153ecc128932e6bd
Usman Shaikh <shaikhusman57@gmail.com>
2022-06-09 23:54:18 +0530

    walkthrough pages styling complete

--- client/2do/src/components/Walkthrough/Slider/SliderContent.js
+++ client/2do/src/components/Walkthrough/Slider/SliderContent.js
@@ -6,13 +6,19 @@ const SliderContent = (props) => {
   return (
     <>
       <div className="sliderContent">
-        <img src={path.img1} alt="walkthrough" className="fluidImg SliderMainImg" />
-        <div className="infoTxtBox">
-          <p className="title">{path.title}</p>
-          <span className="subTitle">{path.subTitle}</span>
+        <div className="infoBox">
+          <div className="content">
+            <img src={path.img1} alt="walkthrough" className="fluidImg sliderMainImg" />
+            <div className="infoTxtBox">
+              <p className="title">{path.title}</p>
+              <span className="subTitle">{path.subTitle}</span>
+            </div>
+          </div>
+        </div>
+        <div className="bgLayerBox">
+          <img src={path.img2} alt="walkthrough" className="fluidImg bgLayer" />
         </div>
       </div>
-      <img src={path.img2} alt="walkthrough" className="fluidImg bgLayer" />
     </>
   );
 };
--- client/2do/src/pages/Walkthrough/Walkthrough.js
+++ client/2do/src/pages/Walkthrough/Walkthrough.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import CustomButton from "../../components/CustomButton/CustomButton";
 import Slider from "../../components/Walkthrough/Slider/Slider";
+import * as Path from "../../utils/constants/routePath.constants";
 import styles from "./Walkthrough.module.scss";
 
 const Walkthrough = () => {
+  const navigate = useNavigate();
+
+  const onGoToLoginPageHandler = () => {
+    navigate(`/${Path.AUTH}/${Path.LOGIN}`);
+  };
+
   return (
     <>
       <div className={styles.walkthroughPageWrapper}>
         <Slider />
         <div className={styles.actionGrpButton}>
           <div className="w100">
-            <CustomButton color="white" name="Get Started" />
-            <CustomButton color="transparent" name="Log In" />
+            <CustomButton color="white" name="Get Started" onClick={onGoToLoginPageHandler} />
           </div>
         </div>
       </div>
