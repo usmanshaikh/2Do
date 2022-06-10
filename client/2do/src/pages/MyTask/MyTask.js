a7cb399188d7e80d4c0b7876ee2b2969ee822b2b
USMAN SHAIKH <usman.shaikh@3i-infotech.com>
2022-06-10 14:34:22 +0530

    MyTask - Tab styling done

--- client/2do/src/pages/MyTask/MyTask.js
+++ client/2do/src/pages/MyTask/MyTask.js
@@ -1,17 +1,17 @@
 import React from "react";
-import styles from "./MyTask.module.scss";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Box from "@mui/material/Box";
 import TabContent from "../../components/MyTask/TabContent";
 import TaskItem from "../../components/MyTask/TaskItem";
+import "./MyTask.scss";
 
 const MyTask = () => {
-  const [value, setValue] = React.useState(0);
+  const [tabTitle, setTabTitle] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  const onChangeTabHandler = (event, newValue) => {
+    setTabTitle(newValue);
   };
+
   const a11yProps = (index) => {
     return {
       id: `full-width-tab-${index}`,
@@ -21,23 +21,23 @@ const MyTask = () => {
 
   return (
     <>
-      <div className={styles.myTaskPageWrapper}>
-        <h1>My Task</h1>
-        <Box sx={{ width: "100%" }}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <Tabs value={value} onChange={handleChange}>
+      <div className="myTaskPageWrapper">
+        <div className="tabWrapper">
+          <div className="tabHeaderbox">
+            <Tabs value={tabTitle} variant="fullWidth" onChange={onChangeTabHandler}>
               <Tab label="Today" {...a11yProps(0)} />
               <Tab label="Month" {...a11yProps(1)} />
             </Tabs>
-          </Box>
-          <TabContent value={value} index={0}>
-            Today
-            {/* <TaskItem /> */}
-          </TabContent>
-          <TabContent value={value} index={1}>
-            Month
-          </TabContent>
-        </Box>
+          </div>
+          <div className="tabContentbox">
+            <TabContent value={tabTitle} index={0}>
+              {/* <TaskItem /> */}
+            </TabContent>
+            <TabContent value={tabTitle} index={1}>
+              {/* <TaskItem /> */}
+            </TabContent>
+          </div>
+        </div>
       </div>
     </>
   );
