f84770def99f5e10aac861edb216e0493abe89f8
USMAN SHAIKH <usman.shaikh@3i-infotech.com>
2022-06-15 13:04:02 +0530

    Add task page template done

--- client/2do/src/pages/AddTask/AddTask.js
+++ client/2do/src/pages/AddTask/AddTask.js
@@ -1,10 +1,61 @@
 import React from "react";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { TextField } from "@mui/material";
+import ChooseColor from "../../components/ChooseColor/ChooseColor";
+import CustomButton from "../../components/CustomButton/CustomButton";
+import ChooseCategory from "../../components/ChooseCategory/ChooseCategory";
+import SetDateTime from "../../components/SetDateTime/SetDateTime";
+import * as Msg from "../../utils/constants/message.constants";
 import "./AddTask.scss";
 
+const validationSchema = yup.object({
+  description: yup.string().required(Msg.DESCRIPTION_REQUIRED),
+});
+
 const AddTask = () => {
+  const formik = useFormik({
+    initialValues: {
+      description: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log({ values });
+    },
+  });
+
+  const onAddQuickNoteHandler = () => {};
+
   return (
     <>
-      <div className="addTaskPageWrapper">AddTask</div>
+      <div className="addTaskPageWrapper">
+        <div className="cardWrapper">
+          <div className="formWrapper">
+            <form onSubmit={formik.handleSubmit}>
+              <div className="commonInputWrap">
+                <span className="commonLabel">description</span>
+                <TextField
+                  fullWidth
+                  multiline
+                  id="description"
+                  name="description"
+                  autoComplete="off"
+                  className="commonInputFormControl"
+                  placeholder="Enter Description"
+                  value={formik.values.description}
+                  onChange={formik.handleChange}
+                  error={formik.touched.description && Boolean(formik.errors.description)}
+                  helperText={formik.touched.description && formik.errors.description}
+                />
+              </div>
+              <ChooseCategory />
+              <SetDateTime />
+              <ChooseColor />
+              <CustomButton name="Done" type="submit" onClick={onAddQuickNoteHandler} />
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
