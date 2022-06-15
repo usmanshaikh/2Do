def585e0d8ca0ee4cbae62712310b348a27fe4d7
USMAN SHAIKH <usman.shaikh@3i-infotech.com>
2022-06-15 18:21:41 +0530

    CategoryCard -  TouchRipple effect added

--- client/2do/src/components/Category/CategoryCard/CategoryCard.js
+++ client/2do/src/components/Category/CategoryCard/CategoryCard.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { CardActionArea, Card } from "@mui/material";
 import "./CategoryCard.scss";
 
 const CategoryCard = (props) => {
   return (
     <>
-      <div className="categoryCard">
-        <span className="circle" style={{ backgroundColor: `${props.color}` }}></span>
-        <span className="title">{props.title}</span>
-        <span className="count-info">
-          <span className="count">{props.count}</span> Tasks
-        </span>
-      </div>
+      <Card className="categoryCardComponentWrapper">
+        <CardActionArea>
+          <div className="cardWrap">
+            <span className="circle" style={{ backgroundColor: `${props.color}` }}></span>
+            <span className="title">{props.title}</span>
+            <span className="count-info">
+              <span className="count">{props.count}</span> Tasks
+            </span>
+          </div>
+        </CardActionArea>
+      </Card>
     </>
   );
 };
