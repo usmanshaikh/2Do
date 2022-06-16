7c44910917aa77b19cec1b46f7f80d0ce19a564a
USMAN SHAIKH <usman.shaikh@3i-infotech.com>
2022-06-16 17:32:28 +0530

    truncateString() modified

--- client/2do/src/utils/Helpers/Helpers.js
+++ client/2do/src/utils/Helpers/Helpers.js
@@ -1,5 +1,12 @@
 export function truncateString(str, number = 90) {
   let modifiedStr = str;
-  if (str.length > number) modifiedStr = `${str.slice(0, number)}...`;
+  if (str.length > number) {
+    str = str.slice(0, number).trim();
+    if (str[str.length - 1] === ".") {
+      str = str.slice(0, -1);
+      // checking if string already have dot then remove.
+    }
+    modifiedStr = `${str}...`;
+  }
   return modifiedStr;
 }
