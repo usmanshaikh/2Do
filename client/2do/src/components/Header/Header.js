3c6b426ce9942e27eddae31371c8134ae869ce12
Usman Shaikh <shaikhusman57@gmail.com>
2022-06-19 18:56:48 +0530

    Icon import rm frm header

--- client/2do/src/components/Header/Header.js
+++ client/2do/src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Icon } from "@mui/material";
+import { Button } from "@mui/material";
 import FilterTaskModal from "../Modals/FilterTaskModal/FilterTaskModal";
 import style from "./Header.module.scss";
 import Images from "../../assets/img/images.js";
