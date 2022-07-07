1d91463811e4cedac2a72f566bfd53eadf20533c
USMAN SHAIKH <usman.shaikh@3i-infotech.com>
2022-07-07 16:42:37 +0530

    Filter Task Modal calling method change with useModal() and action btn added

--- client/2do/src/components/Header/Header.js
+++ client/2do/src/components/Header/Header.js
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "@mui/material";
+import { useModal } from "mui-modal-provider";
 import FilterTaskModal from "../Modals/FilterTaskModal/FilterTaskModal";
 import style from "./Header.module.scss";
 import Images from "../../assets/img/images.js";
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
-  const [selectedValue, setSelectedValue] = React.useState("usman");
+  const { showModal } = useModal();
 
   const openFiterModalHandler = () => {
-    setOpen(true);
+    const initialState = {
+      onFilter: (data) => filterHandler(data),
+    };
+    showModal(FilterTaskModal, initialState, { destroyOnClose: true });
   };
 
-  const closeFiterModalHandler = (value) => {
-    setOpen(false);
-    setSelectedValue(value);
+  const filterHandler = (data) => {
+    console.log({ data });
   };
 
   return (
@@ -29,7 +31,6 @@ const Header = () => {
             <Button className={style.filterBtn} variant="outlined" onClick={openFiterModalHandler}>
               <img src={Images.FilterSVG} alt="filter" className="filterImg" />
             </Button>
-            <FilterTaskModal selectedValue={selectedValue} open={open} onClose={closeFiterModalHandler} />
           </div>
         </div>
       </div>
--- client/2do/src/components/Modals/FilterTaskModal/FilterTaskModal.js
+++ client/2do/src/components/Modals/FilterTaskModal/FilterTaskModal.js
@@ -1,47 +1,100 @@
-import React from "react";
-import { Dialog, DialogTitle, Icon, List, ListItem, ListItemText } from "@mui/material";
+import React, { useState } from "react";
+import { Button, Dialog, DialogActions, DialogTitle, Icon, List, ListItem, ListItemText } from "@mui/material";
 import "./FilterTaskModal.scss";
 
-const FilterTaskModal = (props) => {
-  const { onClose, selectedValue, open } = props;
+const CATEGORY_ITEM = [
+  {
+    label: "personal",
+    id: 1,
+  },
+  {
+    label: "home",
+    id: 2,
+  },
+  {
+    label: "office",
+    id: 3,
+  },
+];
 
-  const handleClose = () => {
-    onClose(selectedValue);
+const FILTER_ITEM = [
+  {
+    label: "all tasks",
+    id: 1,
+  },
+  {
+    label: "incomplete tasks",
+    id: 2,
+  },
+  {
+    label: "completed tasks",
+    id: 3,
+  },
+];
+
+/**
+ *
+ * @param {{ onFilter: () }} props
+ */
+
+const FilterTaskModal = (props) => {
+  const { onClose, open, onFilter } = props;
+  const [categoryBy, setCategoryBy] = useState(CATEGORY_ITEM[0].label);
+  const [filterBy, setFilterBy] = useState(FILTER_ITEM[0].label);
+
+  const onFilterHandler = () => {
+    const payload = {
+      categoryBy,
+      filterBy,
+    };
+    onFilter(payload);
   };
 
-  const handleListItemClick = (value) => {
-    onClose(value);
+  const onCancelHandler = () => {
+    onClose();
+  };
+
+  const categoryByHandler = (data) => {
+    setCategoryBy(data);
+  };
+
+  const filterByHandler = (data) => {
+    setFilterBy(data);
   };
 
   return (
     <>
-      <Dialog className="filterTaskModalComponentWrapper" onClose={handleClose} open={open} fullWidth={true}>
+      <Dialog
+        className="filterTaskModalComponentWrapper commonModalWrapper"
+        onClose={onClose}
+        open={open}
+        fullWidth={true}>
         <DialogTitle className="modalHeading">Category By</DialogTitle>
         <List>
-          <ListItem button onClick={() => handleListItemClick()}>
-            <ListItemText className="filterName" primary="Personal" />
-            <Icon className="mIcon material-icons-round mCheckIcon">check</Icon>
-          </ListItem>
-          <ListItem button onClick={() => handleListItemClick()}>
-            <ListItemText className="filterName" primary="Home" />
-          </ListItem>
-          <ListItem button onClick={() => handleListItemClick()}>
-            <ListItemText className="filterName" primary="Office" />
-          </ListItem>
+          {CATEGORY_ITEM.map((item) => (
+            <ListItem key={item.id} button onClick={() => categoryByHandler(item.label)}>
+              <ListItemText className="filterName" primary={item.label} />
+              {categoryBy === item.label && <Icon className="mIcon material-icons-round mCheckIcon">check</Icon>}
+            </ListItem>
+          ))}
         </List>
         <DialogTitle className="modalHeading">Filter By</DialogTitle>
         <List>
-          <ListItem button onClick={() => handleListItemClick()}>
-            <ListItemText className="filterName" primary="Incomplete tasks" />
-            <Icon className="mIcon material-icons-round mCheckIcon">check</Icon>
-          </ListItem>
-          <ListItem button onClick={() => handleListItemClick()}>
-            <ListItemText className="filterName" primary="Completed tasks" />
-          </ListItem>
-          <ListItem button onClick={() => handleListItemClick()}>
-            <ListItemText className="filterName" primary="All tasks" />
-          </ListItem>
+          {FILTER_ITEM.map((item) => (
+            <ListItem key={item.id} button onClick={() => filterByHandler(item.label)}>
+              <ListItemText className="filterName" primary={item.label} />
+              {filterBy === item.label && <Icon className="mIcon material-icons-round mCheckIcon">check</Icon>}
+            </ListItem>
+          ))}
         </List>
+        <DialogActions className="actionBtnFlexContainer">
+          <Button onClick={onCancelHandler} className="cancelBtn actionBtn">
+            Cancel
+          </Button>
+          <Button variant="contained" onClick={onFilterHandler} className="filterBtn actionBtn successBtn">
+            Filter
+          </Button>
+        </DialogActions>
       </Dialog>
     </>
   );
