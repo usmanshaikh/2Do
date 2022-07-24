ee41856be13e2a8b73fc72c732826592b858a687
Usman Shaikh <shaikhusman57@gmail.com>
2022-07-24 21:02:22 +0530

    Email template code refactor

--- server/2do/src/services/email.service.js
+++ server/2do/src/services/email.service.js
@@ -5,6 +5,9 @@ const hbs = require('nodemailer-express-handlebars');
 const path = require('path');
 const fs = require('fs');
 
+// Current Directory
+const dirPath = path.join(__dirname, '..');
+
 const transport = nodemailer.createTransport(config.email.smtp);
 /* istanbul ignore next */
 if (config.env !== 'test') {
@@ -14,6 +17,29 @@ if (config.env !== 'test') {
     .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
 }
 
+// HBS Config
+const hbsOptions = {
+  viewEngine: {
+    layoutsDir: path.join(dirPath, 'views', 'layouts'),
+    partialsDir: path.join(dirPath, 'views', 'partials'),
+    extname: '.handlebars',
+    defaultLayout: 'main',
+  },
+  extName: '.handlebars',
+  viewPath: path.join(dirPath, 'views'),
+};
+transport.use('compile', hbs(hbsOptions));
+
+/**
+ * Get Email Template Style
+ * @param {string} templateTitle
+ * @returns {Promise}
+ */
+const getEmailTemplateStyle = async () => {
+  let templateStyle = fs.readFileSync(path.resolve(dirPath, './public/css/email-template.css'));
+  return templateStyle.toString();
+};
+
 /**
  * Send an email
  * @param {string} to
@@ -21,36 +47,8 @@ if (config.env !== 'test') {
  * @param {string} text
  * @returns {Promise}
  */
-const sendEmail = async (to, subject, text) => {
-  const msg = { from: config.email.from, to, subject, text };
-  await transport.sendMail(msg);
-};
-
-const sendEmail2 = async (to, subject, text) => {
-  const dir = path.join(__dirname, '..');
-
-  const templateStyleFileContent = fs.readFileSync(path.resolve(dir, './public/css/email-template.css'));
-  const templateStyle = templateStyleFileContent.toString();
-
-  const options = {
-    viewEngine: {
-      layoutsDir: path.join(dir, 'views', 'layouts'),
-      partialsDir: path.join(dir, 'views', 'partials'),
-      extname: '.handlebars',
-      defaultLayout: 'main',
-    },
-    extName: '.handlebars',
-    viewPath: path.join(dir, 'views'),
-  };
-
-  transport.use('compile', hbs(options));
-
-  const templateContent = {
-    templateStyle,
-    templateTitle: 'Reset Password',
-  };
-
-  const msg = { from: config.email.from, to, subject, template: 'resetPasswordEmailTemplate', context: templateContent };
+const sendEmail = async (to, subject, template, context) => {
+  const msg = { from: config.email.from, to, subject, template, context };
   await transport.sendMail(msg);
 };
 
@@ -64,10 +62,14 @@ const sendResetPasswordEmail = async (to, token) => {
   const subject = 'Reset password';
   // replace this url with the link to the reset password page of your front-end app
   const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
-  const text = `Dear user,
-To reset your password, click on this link: ${resetPasswordUrl}
-If you did not request any password resets, then ignore this email.`;
-  await sendEmail(to, subject, text);
+  const templateToUse = 'resetPasswordTemplate';
+  const templateStyle = await getEmailTemplateStyle();
+  const templateContent = {
+    templateStyle,
+    templateTitle: 'Reset Password',
+    resetPasswordUrl,
+  };
+  await sendEmail(to, subject, templateToUse, templateContent);
 };
 
 /**
@@ -80,10 +82,14 @@ const sendVerificationEmail = async (to, token) => {
   const subject = 'Email Verification';
   // replace this url with the link to the email verification page of your front-end app
   const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
-  const text = `Dear user,
-To verify your email, click on this link: ${verificationEmailUrl}
-If you did not create an account, then ignore this email.`;
-  await sendEmail(to, subject, text);
+  const templateToUse = 'emailVerificationTemplate';
+  const templateStyle = await getEmailTemplateStyle();
+  const templateContent = {
+    templateStyle,
+    templateTitle: 'Email Verification',
+    verificationEmailUrl,
+  };
+  await sendEmail(to, subject, templateToUse, templateContent);
 };
 
 module.exports = {
