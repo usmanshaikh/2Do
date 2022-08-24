fed4159b92671fc811ec0de7a495b3cdbfa236d9
Usman Shaikh <shaikhusman57@gmail.com>
2022-08-24 14:31:18 +0530

    UI - All task by Date functionality implment and noDataFound component lottie added

--- client/2do/src/components/DatePickerControl/DatePickerControl.js
+++ client/2do/src/components/DatePickerControl/DatePickerControl.js
@@ -6,29 +6,40 @@ import { DatePicker } from "@mui/x-date-pickers";
 import moment from "moment";
 import "./DatePickerControl.scss";
 
-const DatePickerControl = () => {
-  const [value, setValue] = React.useState(null);
+const DatePickerControl = (props) => {
+  const { selectedDate } = props;
+  const [datePicker, setDatePicker] = React.useState(null);
 
   const minY = moment().startOf("year").format("YYYY-MM-DD");
   const maxY = moment().endOf("year").format("YYYY-MM-DD");
   const calendarMaxDate = moment(new Date(maxY), "DD/MM/YYYY");
   const calendarMinDate = moment(new Date(minY), "DD/MM/YYYY");
 
+  const onSelectDateHandler = (selectedData) => {
+    const dateAndTime = moment(selectedData).toDate();
+    setDatePicker(dateAndTime);
+  };
+
+  const onOkHandler = () => {
+    const obj = { dateAndTime: datePicker };
+    selectedDate(obj);
+  };
+
   return (
     <>
       <div className="calendarComponentWrapper">
         <LocalizationProvider dateAdapter={AdapterMoment}>
           <DatePicker
+            error={false}
             label="Select Date"
             openTo="day"
-            value={value}
-            onChange={(newValue) => {
-              setValue(newValue);
-            }}
+            value={datePicker}
+            onChange={(selectedData) => onSelectDateHandler(selectedData)}
+            onAccept={onOkHandler}
             maxDate={calendarMaxDate}
             minDate={calendarMinDate}
             fullWidth
-            inputFormat="DD/MM/YYYY"
+            inputFormat="DD/MMM/YYYY"
             renderInput={(params) => (
               <TextField className="datePickerInputControl" fullWidth placeholder="DD/MM/YYYY" {...params} />
             )}
