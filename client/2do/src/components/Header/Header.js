f1714a0b57e0b3972593809f198b87c7b900a53d
Usman Shaikh <shaikhusman57@gmail.com>
2022-08-25 07:08:13 +0530

    UI - showing filter btn only in the main page

--- client/2do/src/components/Header/Header.js
+++ client/2do/src/components/Header/Header.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { Button } from "@mui/material";
 import { useModal } from "mui-modal-provider";
+import { useLocation } from "react-router-dom";
 import { FilterTaskModal } from "../Modals";
 import { useGlobalContext } from "../../utils/hooks";
 import Images from "../../assets/img/images.js";
+import constants from "../../utils/constants";
 import "./Header.scss";
 
+const ROUTE = constants.routePath;
+
 const Header = () => {
+  const location = useLocation();
   const { showModal } = useModal();
   const { headerTitle: title } = useGlobalContext();
+  let isShowFilterBtn = false;
 
   const openFiterModalHandler = () => {
-    const initialState = {
-      onFilter: (data) => filterHandler(data),
-    };
-    showModal(FilterTaskModal, initialState, { destroyOnClose: true });
+    showModal(FilterTaskModal, undefined, { destroyOnClose: true });
   };
 
-  const filterHandler = (data) => {
-    console.log({ data });
-  };
-
-  const isShowFilterBtn = title !== "Category" && title !== "Profile";
+  if (location.pathname === `/${ROUTE.TASK}` || location.pathname === `/${ROUTE.CHECKLIST}`) isShowFilterBtn = true;
 
   return (
     <>
