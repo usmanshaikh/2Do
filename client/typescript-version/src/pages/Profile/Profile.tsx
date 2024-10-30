8989b246b973ebefe5077ac50ea88e36e7394b43
Usman Shaikh <shaikhusman57@gmail.com>
2024-10-30 17:48:01 +0530

    client - Profile.tsx code refactor

--- client/typescript-version/src/pages/Profile/Profile.tsx
+++ client/typescript-version/src/pages/Profile/Profile.tsx
@@ -16,7 +16,7 @@ const Profile = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const { showModal } = useModal();
-  const [profile, setProfile] = useState<UserResponse>();
+  const [userProfile, setUserProfile] = useState<UserResponse>();
   const [report, setReport] = useState<StatisticReportResponse>();
 
   useEffect(() => {
@@ -25,158 +25,166 @@ const Profile = () => {
   }, []);
 
   const fetchUserProfile = async () => {
-    const res = await userApi.myProfile();
-    setProfile(res.data);
+    try {
+      const { data } = await userApi.myProfile();
+      setUserProfile(data);
+    } catch (error) {
+      dispatch(showSnackbar({ message: error.data.message || MSG.ERROR_MESSAGE }));
+    }
   };
 
   const fetchStatisticReport = async () => {
-    const res = await userApi.statisticReport();
-    setReport(res.data);
+    try {
+      const { data } = await userApi.statisticReport();
+      setReport(data);
+    } catch (error) {
+      dispatch(showSnackbar({ message: error.data.message || MSG.ERROR_MESSAGE }));
+    }
   };
 
-  const updateMyProfile = async (payload: FormData) => {
-    const res = await userApi.updateMyProfile(payload);
-    setProfile(res.data);
+  const updateUserProfile = async (payload: FormData) => {
+    try {
+      const { data } = await userApi.updateMyProfile(payload);
+      setUserProfile(data);
+    } catch (error) {
+      dispatch(showSnackbar({ message: error.data.message || MSG.ERROR_MESSAGE }));
+    }
   };
 
   const openEditProfileModalHandler = () => {
     const initialState = {
-      name: profile?.name,
-      onSubmitForm: (data: FormData) => updateMyProfile(data),
+      name: userProfile?.name,
+      onSubmitForm: (data: FormData) => updateUserProfile(data),
     };
     showModal(EditProfileModal, initialState, { destroyOnClose: true });
   };
 
-  const sendVerifyEmailLinkHander = () => {
-    authApi.sendVerificationEmail().then(() => {
-      dispatch(
-        showSnackbar({
-          message: MSG.USER_FEEDBACK.VERIFICATION_LINK_SENT,
-          type: "info",
-        })
-      );
-    });
+  const handleSendVerifyEmailLink = async () => {
+    try {
+      await authApi.sendVerificationEmail();
+      dispatch(showSnackbar({ message: MSG.USER_FEEDBACK.VERIFICATION_LINK_SENT, type: "info" }));
+    } catch (error) {
+      dispatch(showSnackbar({ message: error.data.message || MSG.ERROR_MESSAGE }));
+    }
   };
 
-  const onLogoutHandler = async () => {
+  const handleLogout = async () => {
     const { refreshToken } = auth;
     if (refreshToken) {
       try {
         await authApi.logout({ refreshToken });
-        logout();
+        performLogout();
       } catch {
-        logout();
+        performLogout();
       }
     }
   };
 
-  const logout = () => {
+  const performLogout = () => {
     dispatch(clearTokens());
     navigate(`/${ROUTES.LOGIN}`);
   };
 
   return (
-    <>
-      <Box className="profilePageWrapper">
-        <Box className="profileCardWrap">
-          <Box className="userInfo flexContainer ">
-            <Box className="flexItemOne">
-              {profile?.image?.data ? (
-                <Avatar
-                  alt="Avatar"
-                  src={`data:image/png;base64,${profile.image.data}`}
-                  sx={{ width: 60, height: 60 }}
-                />
-              ) : (
-                <Box className="nameInitial">{profile?.name.charAt(0)}</Box>
-              )}
-            </Box>
-            <Box className="flexItemTwo">
-              <p className="name">{profile?.name}</p>
-              <p className="email">{profile?.email}</p>
-              <Box className="editProfileBtnWrap">
-                <IconButton aria-label="delete" className="iconBtn" size="small" onClick={openEditProfileModalHandler}>
-                  <Icon className="manageAccountsIcon">manage_accounts</Icon>
-                </IconButton>
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-        {!profile?.isEmailVerified && (
-          <Box className="emailVerifyCardWrap">
-            <p className="infoTxt">
-              <span className="bold">Your email is not verify yet.</span> Please verify your email to receive
-              notifications releated to Task/Checklist reminder.
-            </p>
-            <Box className="center">
-              <Button variant="outlined" onClick={sendVerifyEmailLinkHander}>
-                Verify Email Address
-              </Button>
-            </Box>
-          </Box>
-        )}
-        {report && (report.taskStatistic[0].count || report.checklistStatistic[0].count) ? (
-          <Box className="statisticCardWrap">
-            <h1 className="heading">Statistic</h1>
-            {report.taskStatistic[0].count !== 0 && (
-              <Box className="taskInfo">
-                <span className="title">Task</span>
-                {report?.taskStatistic?.map((data) => {
-                  return (
-                    <Fragment key={data?.label}>
-                      <Box className="flexContainer">
-                        <span className="label">{data?.label}</span>
-                        <span className="count">{data?.count}</span>
-                      </Box>
-                    </Fragment>
-                  );
-                })}
-              </Box>
+    <Box className="profilePageWrapper">
+      <Box className="profileCardWrap">
+        <Box className="userInfo flexContainer ">
+          <Box className="flexItemOne">
+            {userProfile?.image?.data ? (
+              <Avatar
+                alt="Avatar"
+                src={`data:image/png;base64,${userProfile.image.data}`}
+                sx={{ width: 60, height: 60 }}
+              />
+            ) : (
+              <Box className="nameInitial">{userProfile?.name.charAt(0)}</Box>
             )}
-            {report.checklistStatistic[0].count !== 0 && (
-              <Box className="taskInfo">
-                <span className="title">Checklist</span>
-                {report?.checklistStatistic?.map((data) => {
-                  return (
-                    <Fragment key={data?.label}>
-                      <Box className="flexContainer">
-                        <span className="label">{data?.label}</span>
-                        <span className="count">{data?.count}</span>
-                      </Box>
-                    </Fragment>
-                  );
-                })}
-              </Box>
-            )}
-            <Box className="statisticChartWrap">
-              <Box className="flexContainer">
-                {report.taskStatistic[0].count !== 0 && (
-                  <Box className="flexItem">
-                    <Box className="chartBox">
-                      <StatisticChart report={report?.taskStatistic} />
-                    </Box>
-                    <span className="label">Task</span>
-                    <span className="label mg-0">Completed</span>
-                  </Box>
-                )}
-                {report.checklistStatistic[0].count !== 0 && (
-                  <Box className="flexItem">
-                    <Box className="chartBox">
-                      <StatisticChart report={report?.checklistStatistic} />
-                    </Box>
-                    <span className="label">Checklist</span>
-                    <span className="label mg-0">Completed</span>
-                  </Box>
-                )}
-              </Box>
+          </Box>
+          <Box className="flexItemTwo">
+            <p className="name">{userProfile?.name}</p>
+            <p className="email">{userProfile?.email}</p>
+            <Box className="editProfileBtnWrap">
+              <IconButton aria-label="delete" className="iconBtn" size="small" onClick={openEditProfileModalHandler}>
+                <Icon className="manageAccountsIcon">manage_accounts</Icon>
+              </IconButton>
             </Box>
           </Box>
-        ) : null}
-        <Box className="logoutBtnWrap">
-          <CustomButton name="Logout" onClick={onLogoutHandler} />
         </Box>
       </Box>
-    </>
+      {!userProfile?.isEmailVerified && (
+        <Box className="emailVerifyCardWrap">
+          <p className="infoTxt">
+            <span className="bold">Your email is not verify yet.</span> Please verify your email to receive
+            notifications releated to Task/Checklist reminder.
+          </p>
+          <Box className="center">
+            <Button variant="outlined" onClick={handleSendVerifyEmailLink}>
+              Verify Email Address
+            </Button>
+          </Box>
+        </Box>
+      )}
+      {report && (report.taskStatistic[0].count || report.checklistStatistic[0].count) ? (
+        <Box className="statisticCardWrap">
+          <h1 className="heading">Statistic</h1>
+          {report.taskStatistic[0].count !== 0 && (
+            <Box className="taskInfo">
+              <span className="title">Task</span>
+              {report?.taskStatistic?.map((data) => {
+                return (
+                  <Fragment key={data?.label}>
+                    <Box className="flexContainer">
+                      <span className="label">{data?.label}</span>
+                      <span className="count">{data?.count}</span>
+                    </Box>
+                  </Fragment>
+                );
+              })}
+            </Box>
+          )}
+          {report.checklistStatistic[0].count !== 0 && (
+            <Box className="taskInfo">
+              <span className="title">Checklist</span>
+              {report?.checklistStatistic?.map((data) => {
+                return (
+                  <Fragment key={data?.label}>
+                    <Box className="flexContainer">
+                      <span className="label">{data?.label}</span>
+                      <span className="count">{data?.count}</span>
+                    </Box>
+                  </Fragment>
+                );
+              })}
+            </Box>
+          )}
+          <Box className="statisticChartWrap">
+            <Box className="flexContainer">
+              {report.taskStatistic[0].count !== 0 && (
+                <Box className="flexItem">
+                  <Box className="chartBox">
+                    <StatisticChart report={report?.taskStatistic} />
+                  </Box>
+                  <span className="label">Task</span>
+                  <span className="label mg-0">Completed</span>
+                </Box>
+              )}
+              {report.checklistStatistic[0].count !== 0 && (
+                <Box className="flexItem">
+                  <Box className="chartBox">
+                    <StatisticChart report={report?.checklistStatistic} />
+                  </Box>
+                  <span className="label">Checklist</span>
+                  <span className="label mg-0">Completed</span>
+                </Box>
+              )}
+            </Box>
+          </Box>
+        </Box>
+      ) : null}
+      <Box className="logoutBtnWrap">
+        <CustomButton name="Logout" onClick={handleLogout} />
+      </Box>
+    </Box>
   );
 };
 
