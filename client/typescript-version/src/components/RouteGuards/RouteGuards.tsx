48fd240254ccb19ca17f4b6932fdbda54c8b8d4e
Usman Shaikh <shaikhusman57@gmail.com>
2024-10-31 16:41:21 +0530

    client - useAppSelector

--- client/typescript-version/src/components/RouteGuards/RouteGuards.tsx
+++ client/typescript-version/src/components/RouteGuards/RouteGuards.tsx
@@ -1,16 +1,16 @@
-import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 import { RootState } from "../../store";
 import { ROUTES } from "../../utils/constants";
+import { useAppSelector } from "../../hooks";
 
 // Only accessible by logged-in users
 export const ProtectedRoute = () => {
-  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+  const isLoggedIn = useAppSelector((state: RootState) => state.auth.isLoggedIn);
   return isLoggedIn ? <Outlet /> : <Navigate to={`/${ROUTES.LOGIN}`} replace />;
 };
 
 // Only accessible by guests
 export const GuestRoute = () => {
-  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+  const isLoggedIn = useAppSelector((state: RootState) => state.auth.isLoggedIn);
   return !isLoggedIn ? <Outlet /> : <Navigate to={`/${ROUTES.TASK}`} replace />;
 };
