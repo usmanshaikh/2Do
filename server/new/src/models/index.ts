8e40153282de4148d600d330cfeb7c6f20def1be
Usman Shaikh <shaikhusman57@gmail.com>
2025-01-30 22:12:43 +0530

    update model exports to include Category, Checklist, Scheduler, and Task

--- server/new/src/models/index.ts
+++ server/new/src/models/index.ts
@@ -1,3 +1,7 @@
 import User from './user.model';
+import Category from './category.model';
+import Checklist from './checklist.model';
+import Scheduler from './scheduler.model';
+import Task from './task.model';
 
-export { User };
+export { User, Category, Checklist, Scheduler, Task };
