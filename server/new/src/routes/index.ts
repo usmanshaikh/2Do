f07c8559af0a91215ab324f2215dffd336ccb0ba
Usman Shaikh <shaikhusman57@gmail.com>
2025-02-05 23:15:48 +0530

    feat(routes): add routes for categories, tasks, and checklists

--- server/new/src/routes/index.ts
+++ server/new/src/routes/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import authRoute from './auth.routes';
 import userRoute from './user.routes';
 import healthRoute from './health.routes';
+import categoryRoute from './category.route';
+import checklistRoute from './checklist.route';
+import taskRoute from './task.route';
 
 const router = express.Router();
 
@@ -9,6 +12,18 @@ const defaultRoutes = [
   {
     path: '/auth',
     route: authRoute,
+  },
+  {
+    path: '/categories',
+    route: categoryRoute,
+  },
+  {
+    path: '/tasks',
+    route: taskRoute,
+  },
+  {
+    path: '/checklists',
+    route: checklistRoute,
   },
   {
     path: '/users',
