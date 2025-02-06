5495c3d44de38e5ef7c027f1c3428058f0d049c3
Usman Shaikh <shaikhusman57@gmail.com>
2025-02-06 07:55:57 +0530

    refactor(routes): streamline user route

--- server/new/src/routes/user.routes.ts
+++ server/new/src/routes/user.routes.ts
@@ -5,11 +5,10 @@ import { userValidation } from '../validations';
 
 const router = express.Router();
 
+router.get('/statistic-report', authenticateJWT, userController.statisticReport);
+router.get('/completed-percentage', authenticateJWT, userController.completedPercentage);
 router
   .route('/:userId')
   .get(authenticateJWT, validate(userValidation.getUser), userController.getUser)
   .patch(authenticateJWT, uploadImage.single('image'), validate(userValidation.updateUser), userController.updateUser);
-router.get('/statistic-report', authenticateJWT, userController.statisticReport);
-router.get('/completed-percentage', authenticateJWT, userController.completedPercentage);
-
 export default router;
