d49ebbd64e645c57e45338cacf5bc3833555c340
Usman Shaikh <shaikhusman57@gmail.com>
2025-02-06 08:23:43 +0530

    refactor(user): remove completed percentage endpoint from user controller

--- server/new/src/controllers/user.controller.ts
+++ server/new/src/controllers/user.controller.ts
@@ -36,13 +36,3 @@ export const statisticReport = catchAsync(async (req: Request, res: Response) =>
     data: user,
   });
 });
-
-export const completedPercentage = catchAsync(async (req: Request, res: Response) => {
-  const user = await userService.completedPercentage(req, res);
-  sendResponse({
-    res,
-    statusCode: StatusCodes.OK,
-    message: MESSAGES.USER_COMPLETION_PERCENTAGE_FETCHED,
-    data: user,
-  });
-});
